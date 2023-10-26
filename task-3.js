fc46a80f5ae1c02c983be27ba8749a0d376659fb
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2023-10-26 13:31:34 +0200

    Update hometasks

--- task-3.js
+++ task-3.js
@@ -1,9 +1,14 @@
-function getElementWidth(content, padding, border) {
-    let contentInt = Number.parseFloat(content);
-    let paddingInt = Number.parseFloat(padding);
-    let borderInt = Number.parseFloat(border);
-    return  contentInt + paddingInt * 2 +borderInt * 2;
+function filterArray(numbers, value) {
+    let arr1 = [];
+    for (const num of numbers) {
+        if (num > value) {
+            arr1.push(num);
+        }
+    }
+    return arr1;   
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
